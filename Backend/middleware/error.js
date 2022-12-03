90fa96eaba51ea9b5a50a8597ff37b2ec2e48146
sharmaji2612 <mahendrapal2612@gmail.com>
2022-12-03 22:58:02 +0530

    Admin Authentication Complete

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -1,3 +1,4 @@
+const { JsonWebTokenError, TokenExpiredError } = require('jsonwebtoken');
 const ErrorHandler = require('../utils/errorhandler');
 
 
@@ -13,6 +14,27 @@ module.exports = (err, req, res, next) => {
         const message = `Resource not found .Invalid : ${err.path}`;
         err = new ErrorHandler(message, 400);
     }
+
+    //Mongoose duplicate key error
+
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Wrong JWT Token
+    if (err.name === JsonWebTokenError) {
+        const message = `Json web token is invalid,  Try again `;
+        err = new ErrorHandler(message, 400);
+    }
+
+    //JWT Expire error
+
+    if (err.name === TokenExpiredError) {
+        const message = `Json web token is Expired , Try again `;
+        err = new ErrorHandler(message, 400);
+    }
+
 
     res.status(err.statusCode).json({
         success: false,
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
+const { route } = require('./userRoute');
 
 const router = express.Router();
 
@@ -9,13 +10,15 @@ const router = express.Router();
 router.route("/products").get(getAllProducts)
 
 //This is my post request 
-router.route("/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct)
+router.route("/admin/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct)
 
 //This is my Put Request to update it
-router.route("/product/:id")
+router.route("/admin/product/:id")
     .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
     .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
     .get(getProductDetails)
+
+router.route("/product/:id").get(getProductDetails);
 
 
 module.exports = router;
